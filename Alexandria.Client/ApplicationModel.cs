78b543c11f3e3a6126c25a0b45a25f09639d98e8
Christopher Bennage <christopher@bennage.com>
2010-02-03 17:42:27 -0500

    added reordering of the queue by way of buttons. I looked into a drag-n-drop solution. Not hard, but lots of code.

--- Alexandria.Client/ApplicationModel.cs
+++ Alexandria.Client/ApplicationModel.cs
@@ -69,8 +69,34 @@ namespace Alexandria.Client
             dispatcher.BeginInvoke(action);
         }
 
-        public void ReorderQueue()
+        public void MoveForwardInQueue(BookModel book)
         {
+            var currentIndex = Queue.IndexOf(book);
+            ExecuteQueueReorder(currentIndex, currentIndex - 1);
+        }
+
+        public void MoveBackInQueue(BookModel book)
+        {
+            var currentIndex = Queue.IndexOf(book);
+            ExecuteQueueReorder(currentIndex, currentIndex + 1);
+        }
+
+        private void ExecuteQueueReorder(int oldIndex, int newIndex)
+        {
+            Queue.Move(oldIndex, newIndex);
+
+            //TODO: //TODO: send reorder msg
+        }
+
+        public bool CanMoveForwardInQueue(BookModel book)
+        {
+            return Queue.IndexOf(book) > 0;
+        }
+
+        public bool CanMoveBackInQueue(BookModel book)
+        {
+            var lastIndex = Queue.Count - 1;
+            return Queue.IndexOf(book) < lastIndex;
         }
 
         public void AddToQueue(BookModel book)
