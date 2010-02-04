3475800b5ffa690c9f7cf23d736fc58de3ae728d
Christopher Bennage <christopher@bennage.com>
2010-02-04 14:40:23 -0500

    added the missing subviews for subscriptions details

---
+++ Alexandria.Client/Views/SubscriptionDetailsViews/ChangesPending.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Alexandria.Client.Views.SubscriptionDetailsViews
+{
+    using System.Windows.Controls;
+
+    public partial class ChangesPending : UserControl
+    {
+        public ChangesPending()
+        {
+            InitializeComponent();
+        }
+    }
+}
