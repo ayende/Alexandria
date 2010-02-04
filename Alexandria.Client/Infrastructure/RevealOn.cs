47512b1c05c8005fbb73346bee94071c65ec36a1
Christopher Bennage <christopher@bennage.com>
2010-02-04 01:33:35 -0500

    add and remove buttons are hidden until you mouse over a book

---
+++ Alexandria.Client/Infrastructure/RevealOn.cs
@@ -0,0 +1,39 @@
+namespace Alexandria.Client.Infrastructure
+{
+    using System.Windows;
+    using System.Windows.Media;
+
+    public class RevealOn : DependencyObject
+    {
+        public static readonly DependencyProperty HoverOverParentProperty = DependencyProperty.RegisterAttached(
+            "HoverOverParent",
+            typeof (string),
+            typeof (RevealOn),
+            new PropertyMetadata(null, WhenHoverOverParentChanges));
+
+        private static void WhenHoverOverParentChanges(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var child = d as FrameworkElement;
+            if (child == null) return;
+
+            var parent = VisualTreeHelper.GetParent(child) as FrameworkElement;
+            if (parent == null) return;
+
+            child.Visibility = Visibility.Hidden;
+
+            parent.MouseEnter += delegate { child.Visibility = Visibility.Visible; };
+
+            parent.MouseLeave += delegate { child.Visibility = Visibility.Hidden; };
+        }
+
+        public static string GetHoverOverParent(DependencyObject element)
+        {
+            return (string) element.GetValue(HoverOverParentProperty);
+        }
+
+        public static void SetHoverOverParent(DependencyObject element, object value)
+        {
+            element.SetValue(HoverOverParentProperty, value);
+        }
+    }
+}
